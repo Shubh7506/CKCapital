9da00774bc62bca26c1a16b65e57d431ae5af980
belikebro576 <shubhamjadhavooo22@gmail.com>
2025-07-09 17:28:17 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -8,7 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
 import logging
 
 logging.getLogger("urllib3.connectionpool").setLevel(logging.CRITICAL)
